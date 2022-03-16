3b496e35f6285b88e33793e6d7fdff239d494965
Nishit Patira <npatira@amazon.com>
2022-03-16 19:46:31 +0530

    Minor

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: '', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://main.dw1l2zkq2p9i7.amplifyapp.com/',
