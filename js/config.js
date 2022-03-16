bbab920ef66f4aadc8cb5ca9a9fbc5154034ce2a
Nishit Patira <npatira@amazon.com>
2022-03-16 19:55:25 +0530

    Cognito changes

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_u6B0T5sGS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2vcvapg9rta6chgeu57dhu72bo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://main.dw1l2zkq2p9i7.amplifyapp.com/',
